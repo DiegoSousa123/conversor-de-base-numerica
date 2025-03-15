e9be7957b5a59422e51f67c8c8197df55ba78122
Diego Sousa <diegosousa.wds.oficial@gmail.com>
2025-03-15 13:20:17 -0300

    delete base-context.tsx

--- src/utils/base-context.ts
+++
@@ -1,6 +0,0 @@
-'use client'
-import { createContext } from 'react';
-
-const BaseContext = createContext({baseNum: '2', setBaseNum: (baseNum: string) => {baseNum = baseNum;}});
-
-export default BaseContext;
